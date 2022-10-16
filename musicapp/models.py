a56a53d9a48e17e2800c24f3f61b436c670460c6
Balades <haryoholuwah1@gmail.com>
2022-10-16 21:32:06 +0100

    master

--- musicapp/models.py
+++ musicapp/models.py
@@ -1,6 +1,3 @@
-from email.policy import default
-from tkinter import CASCADE
-from unittest.util import _MAX_LENGTH
 from django.db import models
 from datetime import datetime
 # Create your models here.
@@ -12,12 +9,12 @@ class Artiste(models.Model):
 
 
 class Song(models.Model):
-    artiste_id = models.ForeignKey(Artiste,on_delete=CASCADE)
+    artiste_id = models.ForeignKey(Artiste,on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     date_released = models.DateField(default=datetime.today())
     likes = models.IntegerField()
 
 
 class Lyric(models.Model):
-    song_id = models.ForeignKey(Song,on_delete=CASCADE)
+    song_id = models.ForeignKey(Song,on_delete=models.CASCADE)
     content = models.CharField(max_length=3000)
